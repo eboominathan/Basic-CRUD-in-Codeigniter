40c5d40f9dce877af8ee4e78c1241e3f77eb20c4
Boominathan <eboominathan@gmail.com>
2015-05-31 12:38:12 +0530

    changed baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].'/engg';
+$config['base_url']	= "";//'http://'.$_SERVER['SERVER_NAME'].'/engg';
 
 /*
 |--------------------------------------------------------------------------
